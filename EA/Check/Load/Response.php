31ea2838e0ed70d357306a490f104330fdfb8470
Daniel Jost <info@daniel-jost.de>
2012-07-20 13:32:09 +0200

    added load check

---
+++ EA/Check/Load/Response.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * Copyright (c) 2012, Daniel Jost
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted/provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice, this list
+ *   of conditions and the following disclaimer in the documentation and/or other materials
+ *   provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+class EA_Check_Load_Response extends EA_Check_Abstract_Response
+{
+	protected $fLoad1;
+	protected $fLoad5;
+	protected $fLoad15;
+
+	public function setLoad1($fLoad1)
+	{
+		$this->fLoad1 = (float) $fLoad1;
+	}
+
+	public function getLoad1()
+	{
+		return $this->fLoad1;
+	}
+
+	public function setLoad5($fLoad5)
+	{
+		$this->fLoad5 = (float) $fLoad5;
+	}
+
+	public function getLoad5()
+	{
+		return $this->fLoad5;
+	}
+
+	public function setLoad15($fLoad15)
+	{
+		$this->fLoad15 = (float) $fLoad15;
+	}
+
+	public function getLoad15()
+	{
+		return $this->fLoad15;
+	}
+}
